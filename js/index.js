b9a4964f84a326bdb340c2c4f58db2c8a745af5d
Charles Uland <charles.uland424@topper.wku.edu>
2025-06-20 21:41:59 -0500

    new design with circles no borders

--- js/index.js
+++ js/index.js
@@ -52,7 +52,24 @@ class RPSGame {
 
 
 const canvas = document.getElementById("player-hand");
+const canvas2 = document.getElementById("cpu-hand");
 const ctx = canvas.getContext("2d");
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height;
+let circleRadius = 70;
+ctx.lineWidth = 8;
+ctx.beginPath();
+ctx.arc(canvasWidth / 2, canvasHeight / 2, circleRadius, 0, Math.PI * 2);
+ctx.strokeStyle = "#B1D4E0";
+ctx.stroke();
+
+const ctx2 = canvas2.getContext("2d");
+ctx2.lineWidth = 8;
+ctx2.beginPath();
+ctx2.arc(canvasWidth / 2, canvasHeight / 2, circleRadius, 0, Math.PI * 2);
+ctx2.strokeStyle = "#B1D4E0";
+ctx2.stroke();
+
 const buttonRock = document.getElementById("button1");
 const buttonPaper = document.getElementById("button2");
 const buttonScissors = document.getElementById("button3");
